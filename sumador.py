7e6bbe7c5eb2c59e4d1acd92767037af2146f2bd
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2021-02-12 23:48:13 -0600

    agregando grid a app afinando detalles

--- sumador.py
+++ sumador.py
@@ -9,37 +9,35 @@ import pandas as pd
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
-
-root= tk.Tk()
-root.title('Calculos Consejos')
-
-
 def getExcel ():
     global df
     import_file_path = filedialog.askopenfilename()
     data = pd.read_excel (import_file_path)
     df = pd.DataFrame(data,columns=['No.','TIPO DE CONSEJO','CABECERA','MODALIDAD'])
-    global consejos
-    consejos = df[df.MODALIDAD == 'INDIVIDUAL'].groupby(['TIPO DE CONSEJO','CABECERA']).size()
-
 #crear Vista
 def counter():
-     text.insert(tk.END, str(consejos))
+    text.delete('1.0',tk.END)
+    consejosIndividuales = df[df.MODALIDAD == 'INDIVIDUAL'].groupby(['TIPO DE CONSEJO','CABECERA']).size()
+    consejosAgrupacion = df[df.MODALIDAD == 'AGRUPACION'].groupby(['TIPO DE CONSEJO','CABECERA']).size()
+    head = '========= Consejos Individuales =========\n'
+    text.insert(tk.END, head + str(consejosIndividuales))
+    head = '\n\n========= Consejos Agrupacion =========\n'
+    text.insert(tk.END, head + str(consejosAgrupacion))
     
-    
-    
-# Botones
-browseButton_Excel = tk.Button(text='Importar Excel', command=getExcel, bg='#E12181', fg='white', font=('helvetica', 12, 'bold'))
-count = tk.Button(text='Realizar conteo', command=counter, bg='#E12181', fg='white', font=('helvetica', 12, 'bold'))
+if __name__ == '__main__':
+    #ventana
+    root= tk.Tk()
+    root.title('Calculos Consejos')
+    root.geometry("500x400")
+    root.resizable(0,1)
 
-browseButton_Excel.pack(side=tk.LEFT)
-count.pack(side=tk.RIGHT)
-
-#vista
-text = tk.Text()
-text.pack(side=tk.TOP);
-
-
-
-
-root.mainloop()
+    # Botones
+    browseButton_Excel = tk.Button(text='Importar Excel', command=getExcel, bg='#E12181', fg='white', font=('helvetica', 12, 'bold'))
+    browseButton_Excel.grid(column=0,row=0,sticky="we")
+    count = tk.Button(text='Realizar conteo', command=counter, bg='#E12181', fg='white', font=('helvetica', 12, 'bold'))
+    count.grid(column=1,row=0,sticky="we")
+    #vista
+    text = tk.Text(x=400,y=300)
+    text.grid(column=0,row=1,columnspan=3, sticky="we")
+    #run
+    root.mainloop()
