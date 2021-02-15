cadf7af0786f85ebb8921eef49841bc2d6038200
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2021-02-15 10:07:38 -0600

    correcion de sumador scroll var

--- sumador.py
+++ sumador.py
@@ -9,6 +9,7 @@ import pandas as pd
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+import tkinter.scrolledtext as scrolledtext
 def getExcel ():
     global df
     import_file_path = filedialog.askopenfilename()
@@ -16,13 +17,12 @@ def getExcel ():
     df = pd.DataFrame(data,columns=['No.','TIPO DE CONSEJO','CABECERA','MODALIDAD'])
 #crear Vista
 def counter():
+    modalidades = ['INDIVIDUAL','AGRUPACION']
     text.delete('1.0',tk.END)
-    consejosIndividuales = df[df.MODALIDAD == 'INDIVIDUAL'].groupby(['TIPO DE CONSEJO','CABECERA']).size()
-    consejosAgrupacion = df[df.MODALIDAD == 'AGRUPACION'].groupby(['TIPO DE CONSEJO','CABECERA']).size()
-    head = '========= Consejos Individuales =========\n'
-    text.insert(tk.END, head + str(consejosIndividuales))
-    head = '\n\n========= Consejos Agrupacion =========\n'
-    text.insert(tk.END, head + str(consejosAgrupacion))
+    for modalidad in modalidades:
+        head = f'\n========= CONSEJO MODALIDAD {modalidad} =========\n\n'
+        consejo = df[df.MODALIDAD == modalidad].groupby(['TIPO DE CONSEJO','CABECERA']).size()
+        text.insert(tk.END, head + str(consejo))
     
 if __name__ == '__main__':
     #ventana
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     count = tk.Button(text='Realizar conteo', command=counter, bg='#E12181', fg='white', font=('helvetica', 12, 'bold'))
     count.grid(column=1,row=0,sticky="we")
     #vista
-    text = tk.Text(x=400,y=300)
+    text = scrolledtext.ScrolledText(root, undo=True,x=500,y=400)
     text.grid(column=0,row=1,columnspan=3, sticky="we")
     #run
     root.mainloop()
